ba0a3fca399ff7b9208d286b123baeb706696d01
Sergey Balyoshenko <sirozhkabalyoshenko@gmail.com>
2015-11-02 11:55:26 +0200

    lesson9

---
+++ GoIT/javascript/home/lesson9/task4.js
@@ -0,0 +1,24 @@
+function f(x) {
+    return Math.random()*x;
+}
+
+function makeCaching(f) {
+    var cache = {};
+
+    return function(x) {
+        if (!(x in cache)) {
+            cache[x] = f.call(this, x);
+        }
+        return cache[x];
+    };
+
+}
+
+f = makeCaching(f);
+
+var a = f(1);
+var b = f(1);
+alert( a == b ); // true (значение закешировано)
+
+b = f(2);
+alert( a == b ); // false, другой аргумент => другое значение
